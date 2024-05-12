2b7e64b57b2e258217101c08edd41f56c0584cee
idgaf2133 <155026436+idgaf2133@users.noreply.github.com>
2024-05-12 16:47:07 +1000

    dcided to seperate data into 3 files
    line chart progress

--- script.js
+++ script.js
@@ -1,116 +1,112 @@
+
 function init()
 {
-    // Set the width and height of the SVG container
-    var w = 600;
-    var h = 300;
-    var padding = 55; // Padding for the axes and chart area
+var w = 1000;
+var h = 500;
+var padding = 55;
 
-var svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
-
-var xScale = d3.scaleTime()
-    .domain([
-        d3.min(dataset, function(d) { return d.date; }),
-        d3.max(dataset, function(d) { return d.date; })
-    ])
-    .range([padding, w]);
-
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d) { return d.number; })])
-    .range([h - padding, 0]);
-
-// Define x and y axes
-var xAxis = d3.axisBottom()
-    .ticks(10)
-    .scale(xScale);
-
-var yAxis = d3.axisLeft()
-    .ticks(10)
-    .scale(yScale);
-
-// Define line and area functions
-var line = d3.line()
-    .x(function(d) { return xScale(d.date); })
-    .y(function(d) { return yScale(d.number); });
+ // Select the body element and append an SVG container
 
 
-// Append SVG container to the #chart element
-var svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+ 
 
-// Append area path
-svg.append("path")
-    .datum(dataset)
-    .attr("class", "line")
-    .attr("d", line);
-
-// Append x-axis
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(0," + (h - padding) + ")")
-    .call(xAxis);
-
-// Append y-axis
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(" + padding + ",0)")
-    .call(yAxis);
-
-
-
-    const parseDate = d3.timeParse("%Y");
-
-    let data;
-    // Function to convert CSV row data into the desired format
-    var rowConverter = function(d) {
-        return {
-            year: parseDate(d.Year),
-            value: +d.Value,
-            variable: d.Variable
-        };
-    }
-
-    // Load the CSV data and execute the callback function
-    d3.csv("nibba.csv", rowConverter)
-        .then(function(loadedData) {
-            // Pass the loaded dataset to the lineChart function
-            data = loadedData;
-           updateChart('ACATHEPB');  
-            // Log the dataset to the console
-            //console.table(dataset, ["date", "number"]);
-        });
-
-    
-    function updateChart(varCode){
-
-        var filteredData = data.filter(function(d) {
-            return d.varCode === varCode && d.year.getFullYear() >= 1993;
-        });
-    
-        x.domain([parseDate('1993'), parseDate('2023')]);
-        y.domain([0, d3.max(filteredData, function(d) {
-            return d.value;
-        })]);
-    
-        xAxis.call(d3.axisBottom(x));
-        yAxis.call(d3.axisLeft(y));
-    
-        line.datum(filteredData)
-            .attr("d", d3.line()
-                .x(function(d) {
-                    return x(d.year);
-                })
-                .y(function(d) {
-                    return y(d.value);
-                })
-            );
-    
-    }
+var rowConverter = function(d) {
+    return {
+        date: new Date(+d.year, (+d.month - 1)),
+        number: +d.number
+    };
 }
 
-// Call the init function when the window loads
-window.onload = init;
+
+d3.csv("Unemployment_78-95.csv", rowConverter)
+    .then(function(data) 
+        {
+     var dataset = data;
+     lineChart(dataset);
+
+     console.table(dataset, ["date", "number"]);
+     });
+
+function lineChart(dataset){
+
+    var xScale = d3.scaleTime()
+    .domain([
+         d3.min(dataset, function(d) { return d.date; }),
+         d3.max(dataset, function(d) { return d.date; })
+     ])
+    .range([padding, w]);
+
+      var yScale = d3.scaleLinear()
+     .domain([0, d3.max(dataset, function(d) { return d.number; })])
+     .range([h-padding, 0]);
+
+
+     				
+	var xAxis = d3.axisBottom()
+                 .ticks(10)
+                .scale(xScale);
+                
+                
+     
+     var yAxis = d3.axisLeft()
+                 .ticks(10)
+                .scale(yScale);
+
+    
+    
+
+     var line = d3.line()
+			.x(function(d) { return xScale(d.date); })
+			.y(function(d) { return yScale(d.number); });
+
+    
+
+     area = d3.area()
+             
+            .x(function(d) { return xScale(d.date); })
+            .y0(function() { return yScale.range()[0]; })
+            .y1(function(d) { return yScale(d.number); });
+
+            var svg = d3.select("#chart")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+
+            svg.append("path")
+                .datum(dataset)
+                .attr("class", "line")
+                .attr("d", line);
+            
+            svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(0," + (h - padding) + ")")
+                .call(xAxis);
+
+            svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(" + padding + ",0)")
+                .call(yAxis);
+            /*
+
+            svg.append("line")
+                .attr("class", "line halfMilMark")
+                .attr("x1", padding)
+                .attr("x2", w)
+                .attr("y1", yScale(500000))
+                .attr("y2", yScale(500000));
+
+            //Label 350 ppm line
+            svg.append("text")
+                .attr("class", "halfMillLabel")
+                .attr("x", padding + 10)
+                .attr("y", yScale(500000) - 7)
+                .text("Half a million unemployed");
+         */
+
+    
+
+};
+}
+
+    window.onload = init;
