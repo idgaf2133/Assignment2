3e6cdc7835b0b8382999dad0b7e96a8f639ed5c0
idgaf2133 <155026436+idgaf2133@users.noreply.github.com>
2024-05-23 16:25:36 +1000

    Sorted out the files for incidence
    added the tool tips for the incidence lines as well

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
 
-function init() {
-  
-    loadVisualization('Files/Diptheria/Diptheria.csv', 'Files/Diptheria/Diptheria_incidence.csv', 'blue', 'red', 'DTP'); // Load default visualization on start
-}
+ loadVisualization('Files/Diptheria/Diptheria.csv', 'Files/Diptheria/Diptheria_incidence.csv', 'blue', 'red', 'DTP'); // Load default visualization on start
 
-window.onload = init;
 
 
 function loadVisualization(file1, file2, color1, color2, disease) {
-    var w = 500;
-    var h = 250;
+    var w = 700;
+    var h = 350;
     var padding = 30;
     
     // Importing events data for tooltips
     var immunizationEventsData;
+    var incidenceEventsData;
     if (disease === 'DTP') {
         immunizationEventsData = 'Files/Diptheria/DTPImmunizationEvents.json';
+        incidenceEventsData = 'Files/Diptheria/DTPIncidenceEvents.json'; 
     } else if (disease === 'Hepatitis') {
         immunizationEventsData = 'Files/Hepatitis/HepatitisImmunizationEvents.json';
+        incidenceEventsData = 'Files/Hepatitis/HepatitisIncidenceEvents.json';
     } else if (disease === 'Measles') {
         immunizationEventsData = 'Files/Measels/MeaselsImmunizationEvents.json';
+        incidenceEventsData = 'Files/Measels/MeaselsIncidenceEvents.json';
     }
 
     // Row converter remains the same
@@ -47,11 +47,13 @@ function loadVisualization(file1, file2, color1, color2, disease) {
     Promise.all([
         d3.csv(file1, rowConverter),
         d3.csv(file2, rowConverter),
-        d3.json(immunizationEventsData)
+        d3.json(immunizationEventsData),
+        d3.json(incidenceEventsData)
     ]).then(function(data) {
         var dataset1 = data[0];
         var dataset2 = data[1];
         var immunizationEvents = data[2];
+        var incidenceEvents = data[3];
 
         var xScale = d3.scaleTime()
             .domain([d3.min(dataset1.concat(dataset2), d => d.date), d3.max(dataset1.concat(dataset2), d => d.date)])
@@ -80,29 +82,85 @@ function loadVisualization(file1, file2, color1, color2, disease) {
             .x(d => xScale(d.date))
             .y(d => yScaleRight(d.number));
 
+            /*
+                    var leftPath = svg.append("path")
+            .datum(dataset1)
+            .attr("class", "line")
+            .style("stroke", immunizationColor)
+            .style("fill", "none")
+            .attr("d", lineLeft);
+
+        leftPath.transition()
+            .duration(1000)
+            .attr("d", lineLeft(dataset1));
+
+        var rightPath = svg.append("path")
+            .datum(dataset2)
+            .attr("class", "line")
+            .style("stroke", incidenceColor)
+            .style("fill", "none")
+            .attr("d", lineRight);
+
+        rightPath.transition()
+            .duration(1000)
+            .attr("d", lineRight(dataset2));
+
+            */
         // Draw the lines
         svg.append("path").datum(dataset1).attr("class", "line").style("stroke", color1).style("fill", "none")  .attr("d", lineLeft);
+
+       
+       
+      
         svg.append("path").datum(dataset2).attr("class", "line").style("stroke", color2).style("fill", "none")  .attr("d", lineRight);
+      
+
 
         // Add axes
         svg.append("g").attr("transform", `translate(0,${h - padding})`).call(xAxis);
         svg.append("g").attr("transform", `translate(${padding},0)`).call(yAxisLeft);
         svg.append("g").attr("transform", `translate(${w - padding},0)`).call(yAxisRight);
 
-        // Add circles and tooltips
         dataset1.forEach(function(d) {
             var year = d.date.getFullYear();
             if (immunizationEvents[year]) {
                 svg.append("circle")
                     .attr("cx", xScale(d.date))
                     .attr("cy", yScaleLeft(d.number))
-                    .attr("r", 4)
+                    .attr("r", 5)
                     .style("fill", color1)
                     .on("mouseover", function(event) {
                         tooltip.transition()
                             .duration(300)
-                            .style("opacity", .9);
+                            .style("opacity", .9)
+                            .style("background-color", "lightsteelblue"); // Tooltip background color for dataset1
                         tooltip.html("<strong>Year:</strong> " + year + "<br/><strong>Event:</strong> " + immunizationEvents[year].events + "<br/><strong>Description:</strong> " + immunizationEvents[year].description)
+                            .style("left", (event.pageX) + "px")
+                            .style("top", (event.pageY - 28) + "px");
+                    })
+                    .on("mouseout", function(d) {
+                        tooltip.transition()
+                            .duration(300)
+                            .style("opacity", 0);
+                    });
+            }
+        });
+        
+        // Add circles and tooltips for dataset2
+        dataset2.forEach(function(d) {
+            var year = d.date.getFullYear();
+            if (incidenceEvents[year]) {
+                svg.append("circle")
+                    .attr("cx", xScale(d.date))
+                    .attr("cy", yScaleRight(d.number))
+                    .attr("r", 5)
+                    .style("fill", color2)
+                    .on("mouseover", function(event) {
+                        tooltip.transition()
+                            .duration(300)
+                            .style("opacity", .9)
+                            .style("background-color", "lightcoral"); // Tooltip background color for dataset2
+                        tooltip.html("<strong>Year:</strong> " + year + "<br/><strong>Event:</strong> " + incidenceEvents[year].Event + "<br/><strong>Description:</strong> " + incidenceEvents[year].Description)
                             .style("left", (event.pageX) + "px")
                             .style("top", (event.pageY - 28) + "px");
                     })
